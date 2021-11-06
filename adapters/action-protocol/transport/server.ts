d9f481406982f145437afec9ed866f9fadeb152a
julioacontreras <julio.aranis.contreras@gmail.com>
2021-11-06 15:07:57 +0100

    add secury access in server

--- adapters/action-protocol/transport/server.ts
+++ adapters/action-protocol/transport/server.ts
@@ -1,8 +1,10 @@
 import {Action} from './action';
+import {SecurityAccess} from '../security-access';
 
 export interface ProtocolServerAdapter {
   create: (
     actions: Map<string, Action>,
+    securityAccess: SecurityAccess,
     app: any
   ) => void;
 }
