83a7ca127e44affd4d101d8d09e9900bae28b67c
julioacontreras <julio.aranis.contreras@gmail.com>
2021-11-06 15:17:05 +0100

    add paramconfig in server

--- adapters/action-protocol/transport/server.ts
+++ adapters/action-protocol/transport/server.ts
@@ -4,6 +4,7 @@ import {SecurityAccess} from '../security-access';
 export interface ProtocolServerAdapter {
   create: (
     actions: Map<string, Action>,
+    config: unknown,
     securityAccess: SecurityAccess,
     app: any
   ) => void;
